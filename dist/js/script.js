abe35f40c17f79accdc536526256e6c4aad34049
Hussein0987 <87342160+Hussein0987@users.noreply.github.com>
2022-02-09 00:22:40 +0200

    Add files via upload
    add files

---
+++ dist/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#light-slider").lightSlider({
+        loop:true,
+        auto : true,
+        slideEndAnimation: true,
+        autoWidth: true
+    });
+});
